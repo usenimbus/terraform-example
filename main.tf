968e7165fe9ce88d610ca31dd54f9a64004c06d7
Thomas McFarlane <thomas@usenimbus.com>
2022-10-28 19:46:06 +0000

    try more filters

--- main.tf
+++ main.tf
@@ -64,15 +64,15 @@ data "aws_ami" "ubuntu" {
   most_recent = true
   filter {
     name   = "name"
-    values = ["focal"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
   filter {
-    name   = "tag:ubuntu-version"
-    values = ["focal"]
+    name   = "architecture"
+    values = ["x86_64"]
   }
   owners = ["099720109477"] # Canonical
 }
