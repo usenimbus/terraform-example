66d4cae9fdd5524a438bc471dfdee1444ea2ff58
Thomas McFarlane <thomas@usenimbus.com>
2022-11-08 00:45:59 +0000

    chore: set availability zone

--- main.tf
+++ main.tf
@@ -80,8 +80,12 @@ resource "aws_default_vpc" "default" {
   }
 }
 
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
 resource "aws_default_subnet" "default" {
-  availability_zone = "eu-west-2a"
+  availability_zone = data.aws_availability_zones.available.names[0]
 
   tags = {
     Name = "Default subnet for eu-west-2a"
